443e36258e96c6917e90e59caf2e6386e9553921
Martin Thomson <martin.thomson@gmail.com>
2015-06-12 14:46:49 -0700

    Updating

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-exec rsync -z -r --ignore-existing \
+exec rsync -z -r \
      --exclude='index.*' --exclude=.htaccess --exclude='*~' --exclude='*.rdf' \
      rsync3.xml.resource.org::xml2rfc.bibxml .
