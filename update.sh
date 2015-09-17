4ce3d405847a49d1e011ff66538b5fc6885e9c7c
Martin Thomson <martin.thomson@gmail.com>
2015-09-17 10:07:36 -0700

    Adding autocommit to update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
-exec rsync -z -r \
+cd $(dirname $0)
+git fetch origin
+git checkout gh-pages
+git reset origin/gh-pages --hard
+rsync -z -r \
      --exclude='index.*' --exclude=.htaccess --exclude='*~' --exclude='*.rdf' \
      rsync3.xml.resource.org::xml2rfc.bibxml .
+git add -A .
+git commit -m Update
+[ $1 = "-p" ] && git push origin gh-pages
